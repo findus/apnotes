9e478901a2290edbfcca9cf0893e70f3f6c69728
Philipp <philipp@f1ndus.de>
2019-11-14 17:57:20 +0100

    initial commit

---
+++ src/imap.rs
@@ -0,0 +1,16 @@
+use std::io::{stdout, Write};
+
+use curl::easy::Easy;
+
+// Print a web page onto stdout
+pub fn hello_world_curl() {
+    let mut easy = Easy::new();
+    easy.url("https://www.rust-lang.org/").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    println!("{}", easy.response_code().unwrap());
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+extern crate gtk;
+extern crate gio;
+extern crate curl;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use gtk::{Application, ApplicationWindow, Button, StackSidebarBuilder, Stack, ListBox, Orientation};
+use std::borrow::Borrow;
+use std::fmt::Display;
+
+mod imap;
+
+
+fn main() {
+    let application = Application::new(
+        Some("de.f1ndus.imap-notes"),
+        Default::default(),
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("Notes");
+        window.set_default_size(350, 70);
+
+        let button = Button::new_with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+
+        let button2 = Button::new_with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+
+
+        let boxx = gtk::Box::new(Orientation::Vertical, 2);
+        boxx.add(&button);
+        boxx.add(&button2);
+
+        window.add(&boxx);
+
+
+        window.show_all();
+    });
+
+    imap::hello_world_curl();
+
+    application.run(&[]);
+}
