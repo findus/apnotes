32a999b3fd31c03bf6e170362d0709364d96112f
Philipp Hentschel <philipp@f1ndus.de>
2021-04-06 12:26:30 +0200

    The html converter now inserts the Apple-dash-list class to unordered lists, so that these lists are editable via the original editors

--- lib/src/converter.rs
+++ lib/src/converter.rs
@@ -11,5 +11,6 @@ pub fn convert_to_html(input: &Body) -> String {
     let parser = Parser::new(&input.text.as_ref().expect("Expected body with message"));
     let mut html_output: String = String::new();
     html::push_html(&mut html_output, parser);
+    let mut html_output = html_output.replace("<ul>","<ul class=\"Apple-dash-list\">");
     quoted_printable::encode_to_str(html_output)
 }
